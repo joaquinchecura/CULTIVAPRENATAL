a23e733619b79e9fda423c5b63298950d8f7a898
joaquinchecura <joaquinchecura@gmail.com>
2026-06-09 11:26:52 -0300

    53

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>,
 )
